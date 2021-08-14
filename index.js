26608663622a882729c9fbfd82eb36b374ce74d2
Michael Newman <therealmichaelnewman@gmail.com>
2021-08-14 13:42:24 -0400

    commit tree e7963ee55b0b22b5d161643da302ffb64ed143eb

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function superbowlWin (record){
+    const foundRecord = record.find(record => record.result === 'W');
+    if (foundRecord) {
+        return foundRecord.year;
+    }
+}
+
+superbowlWin(record)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
